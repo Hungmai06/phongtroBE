957a2699a5feb7d7b667801176ee24c582bd624a
Lê Văn Duy <146688378+Duydeptrai17@users.noreply.github.com>
2025-11-12 17:43:10 +0700

    fix

--- src/main/java/com/example/room/mapper/InvoiceMapper.java
+++ src/main/java/com/example/room/mapper/InvoiceMapper.java
@@ -5,7 +5,9 @@ import com.example.room.model.Invoice;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", 
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = {PaymentMapper.class, ContractMapper.class})
 public interface InvoiceMapper {
     InvoiceResponse toResponse(Invoice invoice);
 }
--- src/main/java/com/example/room/mapper/PaymentMapper.java
+++ src/main/java/com/example/room/mapper/PaymentMapper.java
@@ -5,11 +5,15 @@ import com.example.room.dto.response.PaymentResponse;
 import com.example.room.model.Payment;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named; 
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentMapper {
 
     @Mapping(source = "booking.id", target = "bookingId")
+    
+    @Mapping(source = "booking.room.id", target = "roomId")
+    @Named("mapPaymentToResponse") 
     PaymentResponse toResponse(Payment payment);
 }
